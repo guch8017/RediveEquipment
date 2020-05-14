6681de374e082b80dfac25949c8eb18e52a2c892
Ayajirin <493987054@qq.com>
2020-05-15 02:07:03 +0800

    transplant lpsolve into Equipment Solver

--- app/src/main/java/com/guch8017/rediveEquipment/equipsolver/EquipSolver.java
+++ app/src/main/java/com/guch8017/rediveEquipment/equipsolver/EquipSolver.java
@@ -8,18 +8,21 @@ import android.util.SparseLongArray;
 
 import androidx.annotation.Nullable;
 
-import org.apache.commons.math3.optim.PointValuePair;
-import org.apache.commons.math3.optim.linear.LinearConstraint;
-import org.apache.commons.math3.optim.linear.LinearConstraintSet;
-import org.apache.commons.math3.optim.linear.LinearObjectiveFunction;
-import org.apache.commons.math3.optim.linear.NonNegativeConstraint;
-import org.apache.commons.math3.optim.linear.Relationship;
-import org.apache.commons.math3.optim.linear.SimplexSolver;
-import org.apache.commons.math3.optim.nonlinear.scalar.GoalType;
+//import org.apache.commons.math3.optim.PointValuePair;
+//import org.apache.commons.math3.optim.linear.LinearConstraint;
+//import org.apache.commons.math3.optim.linear.LinearConstraintSet;
+//import org.apache.commons.math3.optim.linear.LinearObjectiveFunction;
+//import org.apache.commons.math3.optim.linear.NonNegativeConstraint;
+//import org.apache.commons.math3.optim.linear.Relationship;
+//import org.apache.commons.math3.optim.linear.SimplexSolver;
+//import org.apache.commons.math3.optim.nonlinear.scalar.GoalType;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
+
+import lpsolve.LpSolve;
 
 /**
  * 装备求解器
@@ -62,58 +65,94 @@ public class EquipSolver {
             }
         }
 
-        // 建立线性规划目标函数
-        double[] linearFuncTarget = new double[mapCount];
-        for (int i = 0;i < linearFuncTarget.length;++i)
-            linearFuncTarget[i] = 1.0;
-        LinearObjectiveFunction linearFunc = new LinearObjectiveFunction(linearFuncTarget, 0);
+        try {
+            // 建立线性规划目标函数实体
+            LpSolve lp;
+            int retN = 0;
 
-        // 建立基本的大于0的平凡约束
-        Collection<LinearConstraint> constraints = new ArrayList<LinearConstraint>();
-/* 自带非负约束，故删除此项
-        for (int i = 0;i < mapCount;++i){
-            double[] cons = new double[mapCount];
-            cons[i] = 1.0;
-            constraints.add(new LinearConstraint(cons, Relationship.GEQ, 0));
-        }
-*/
-        // 根据needList, 建立线性规划的约束并求解
-        for (int i = 0;i < needListIdNumMap.size(); ++i){
-            long nowEquipId = needListIdNumMap.keyAt(i);
-            // 遍历这件装备所出现的所有地图
-            EquipDropCapsule[] maps = dropMatrix.getMaps(nowEquipId);
-            if (maps == null) continue;
-            // 建立约束方程组
-            double[] cons = new double[mapCount];
-            for (EquipDropCapsule map : maps) {
-                long nowMapId = map.getMapQuestId();
-                if (mapId2xIndex.indexOfKey(nowMapId) >= 0) {
+            lp = LpSolve.makeLp(0, mapCount);
+            if (lp.getLp() == 0)
+                retN = 1; /* couldn't construct a new model... */
 
-                    int xIndex = mapId2xIndex.get(nowMapId);
-                    cons[xIndex] += map.getDropProb();
+            if (retN == 0){
+                int[] colno = new int[mapCount];
+                for (int i = 1; i <= mapCount;++i){
+                    colno[i - 1] = i;
+                }
+                // row变量存储每一行的系数
+                double[] row = new double[mapCount];
+                lp.setAddRowmode(true);  /* makes building the model faster if it is done rows by row */
+                // 对每一件装备,建立一行新的约束
+                for (int i = 0;i < needListIdNumMap.size();++i){
+                    Arrays.fill(row, 0);
+                    long nowEquipId = needListIdNumMap.keyAt(i);
+                    // 遍历这件装备所出现的所有地图
+                    EquipDropCapsule[] maps = dropMatrix.getMaps(nowEquipId);
+                    if (maps == null) continue;
+                    for (int j = 0;j < maps.length;++j) {
+                        EquipDropCapsule map = maps[j];
+                        long nowMapId = map.getMapQuestId();
+                        if (mapId2xIndex.indexOfKey(nowMapId) >= 0) {
+
+                            int xIndex = mapId2xIndex.get(nowMapId);
+                            row[xIndex] = map.getDropProb();
+                        }
+                    }
+                    lp.addConstraintex(mapCount, row, colno, LpSolve.GE, needListIdNumMap.valueAt(i));
                 }
             }
-            constraints.add(new LinearConstraint(cons, Relationship.GEQ, needListIdNumMap.valueAt(i)));
-        }
 
-        long stTime = System.currentTimeMillis();
-        // 求解
-        PointValuePair solution;
-        // 改为自带非负约束
-        NonNegativeConstraint negativeConstraint = new NonNegativeConstraint(true);
-        solution = (new SimplexSolver()).optimize(linearFunc, new LinearConstraintSet(constraints), GoalType.MINIMIZE, negativeConstraint);
-        if (solution == null)
-            return null;
-        double[] solAns = solution.getPoint();
-        long edTime = System.currentTimeMillis();
-        Log.i(TAG, "SingleSolve: Solve Finish. Time cost " + (edTime-stTime) + "ms");
-        // 包装结果
-        for (int i = 0;i < solAns.length;++i){
-            if (solAns[i] != 0){
-                if(solAns[i] == 0) continue; // 跳过为0的项
-                EquipDropMapCapsule temp = new EquipDropMapCapsule(xIndex2mapId.get(i), solAns[i]);
-                ans.add(temp);
+            if (retN == 0){
+                lp.setAddRowmode(false); /* rowmode should be turned off again when done building the model */
+
+                int[] colno = new int[mapCount];
+                // row变量存储每一行的系数
+                double[] row = new double[mapCount];
+
+                for (int i = 1;i <= mapCount;++i){
+                    colno[i - 1] = i;
+                }
+                Arrays.fill(row,1);
+
+                /* set the objective in lp_solve */
+                lp.setObjFnex(mapCount, row, colno);
+                for (int i = 0;i < mapCount;++i){
+                    lp.setLowbo(i + 1, 0);
+                }
+                lp.setEpslevel(0);  // EPS_TIGHT (0)
             }
+
+            if (retN == 0){
+                /* set the object direction to minimize */
+                lp.setMinim();
+                /* Now let lp_solve calculate a solution */
+                retN = lp.solve();
+                if(retN == LpSolve.OPTIMAL || retN == LpSolve.INFEASIBLE)
+                    retN = 0;
+                else
+                    retN = 5;
+            }
+
+            if (retN == 0){
+                // result变量存储结果
+                double[] result = new double[mapCount];
+                /* variable values */
+                lp.getVariables(result);
+                for (int i = 0;i < mapCount;++i){
+                    if (result[i] != 0){
+                        if(result[i] == 0) continue; // 跳过为0的项
+                        EquipDropMapCapsule temp = new EquipDropMapCapsule(xIndex2mapId.get(i), result[i]);
+                        ans.add(temp);
+                    }
+                }
+            }
+
+            /* clean up such that all used memory by lp_solve is freeed */
+            if(lp.getLp() != 0)
+                lp.deleteLp();
+
+        } catch (Exception e){
+            e.printStackTrace();
         }
 
         ans.sort(new EquipDropMapCapsuleComp());
